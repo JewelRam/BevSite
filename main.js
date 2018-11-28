6b041832baa3b1f30e00ff7587868ec8c1904b6a
Jewel Ramirez <chimpy4u@yahoo.com>
2018-11-28 13:17:48 -0800

    added to main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const loadNews = document.querySelector("#news");
+
+loadNews.addEventListener("click", () => {populateNews(myNewsfeed)});
+
+const loadBev = document.querySelector("#financial");
+
+loadBev.addEventListener("click", () => {populateOther(myNewsfeed)});
+
+// document.querySelector("#financial").addEventListener("click", financialPageFunc);
+loadSummary();
+
+document.querySelector("#travel").addEventListener("click", travelListBuilder);
+
+document.querySelector("#summary").addEventListener("click", loadSummary);
